8af0d1722aca587dd1223e75b339d531f4a29f42
terrellwall <hollywood.starya@gmail.com>
2015-02-19 09:31:59 -0500

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,6 @@
 $("#main").append("Jamaal Ford");
+var formattedName = HTMLheaderName.replace ("%data%", "Jamaal Ford");
+$("#header")append.(formattedName);
+var formattedRole = HTMLheaderRole.replace ("%data%", "Web Developer");
+$("#header")prepend.(formattedRole);
+
